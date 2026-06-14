919ec1cac36f425bfefcd28973f745635467e346
Shay Read <shay.read33@gmail.com>
2026-06-14 00:45:08 -0700

    Update BirdIdentificationsManager to be consistent with new SQL table schema

--- database/manager/bird_identifications_manager.py
+++ database/manager/bird_identifications_manager.py
@@ -7,14 +7,17 @@ Database manager for the bird_identifications table. Contains database lifecycle
 
 class BirdIdentificationsManager:
 
-    INSERT_QUERY_FORMAT = "INSERT INTO bird_identifications (bird_species, location, weather, temperature_fahrenheit) VALUES (?, ?, ?, ?)"
+    INSERT_QUERY_FORMAT = """
+        INSERT INTO bird_identifications (bird_species, location, weather, temperature_fahrenheit, year, month, day, hour)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    """
 
     def __init__(self):
         self.db_connection = sqlite3.connect(constants.TABLE_STORAGE_PATH)
         self.db_cursor = self.db_connection.cursor()
 
-    def create_identification(self, bird_species, location, weather, temperature_farenheit):
-        entry_data = (bird_species, location, weather, temperature_farenheit)
+    def create_identification(self, bird_species, location, weather, temperature_farenheit, year, month, day, hour):
+        entry_data = (bird_species, location, weather, temperature_farenheit, year, month, day, hour)
         self.db_cursor.execute(self.INSERT_QUERY_FORMAT, entry_data)
         self.db_connection.commit()
 
