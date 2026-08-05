7a4ece2849e1906a04dfb3d60685c7bc845f55cd
Shay Read <shay.read33@gmail.com>
2026-08-05 15:11:51 -0700

    Update constants file format

--- util/constants.py
+++ util/constants.py
@@ -1,6 +1,4 @@
-'''
-Shared constants
-'''
+# Shared constants
 
 TABLE_STORAGE_PATH = "database/table/storage/bird_identifications.db"
 LATEST_AUDIO_CLIP_PATH = "audio_processor/storage/latest_audio_clip.wav"
